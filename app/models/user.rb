b91c13d2a208ce3361366e3dd68830766beac92c
Federico Luna <fluna1973@yahoo.com>
2022-11-24 13:13:21 -0600

    validations setup

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
   has_many :comments
   has_many :posts
   has_many :likes
+  validates :name, presence: { message: 'must be given please' }
+  validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   def recent_post
     posts.order('created_at Desc').limit(3)
