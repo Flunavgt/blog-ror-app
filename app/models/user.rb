e043fb78c4fcfdf34f4ac0833c63bee957f08559
Federico Luna <fluna1973@yahoo.com>
2022-11-25 09:38:27 -0600

    Add tests and vaidations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,8 @@ class User < ApplicationRecord
   has_many :comments
   has_many :posts
   has_many :likes
-  validates :name, presence: { message: 'must be given please' }
-  validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :name, presence: true
+  validates :posts_counter, comparison: { greater_than_or_equal_to: 0 }
 
   def recent_post
     posts.order('created_at Desc').limit(3)
