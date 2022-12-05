5100c0dd834b416bb82bdddafca3c37b1e929be1
Federico Luna <fluna1973@yahoo.com>
2022-12-05 17:29:29 -0600

    clean code

--- spec/integration/user_show_spec.rb
+++ spec/integration/user_show_spec.rb
@@ -28,34 +28,3 @@ RSpec.describe 'user_path', type: :system do
     end
   end
 end
-
-# require 'rails_helper'
-
-# RSpec.describe 'Author show page', type: :feature do
-#   before(:each) do
-#     first_user = User.create(name: 'Tom', photo: 'https://picsum.photos/seed/picsum/536/354',
-#                              bio: 'Teacher from Mexico.')
-#     second_user = User.create(name: 'Lilly', photo: 'https://picsum.photos/536/354',
-#                               bio: 'Teacher from Poland.')
-
-#     first_post = Post.create(author: first_user, title: 'Hello', text: 'This is my first post')
-#     second_post = Post.create(author: second_user, title: 'My Second', text: 'I am so nervous')
-
-#     Comment.create(post: first_post, author: second_user, text: 'Hi Tom!')
-#     Comment.create(post: second_post, author: second_user, text: 'I have seen your post')
-
-#     describe 'show page' do
-#       it 'see the user\'s profile picture.' do
-#         visit 'http://localhost:3000/users/2'
-#         images = page.all('img')
-#         expect(images.count).to eq(1)
-#       end
-
-#       describe 'show page' do
-#         it 'see the user\'s profile picture.' do
-#           images = page.all('img')
-#           expect(images.count).to eq(1)
-#         end
-#     end
-#   end
-# end
