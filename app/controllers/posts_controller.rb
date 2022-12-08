799d755307aa1596c5ff17a8429a60cd28e3882d
Federico Luna <fluna1973@yahoo.com>
2022-12-08 08:43:40 -0600

    add authorize resource

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+  load_and_authorize_resource
+
   def index
     @user = User.find(params[:user_id])
     @posts = Post.includes(comments: [:author]).where(author_id: params[:user_id])
@@ -37,5 +39,14 @@ class PostsController < ApplicationController
     user.posts_counter -= 1
     user.save
     redirect_to user_url(user)
+  end
+
+  private
+
+  def post_params
+    params
+      .require(:post)
+      .permit(:title, :text)
+      .with_defaults(author_id: current_user.id)
   end
 end
